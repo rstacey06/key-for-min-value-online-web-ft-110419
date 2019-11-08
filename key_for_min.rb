4901495fc15dfdf2a05bdfac83600dfbff6755fc
Burbage Stacey <rstacey06@gmail.com>
2019-11-08 17:27:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(hash)
   smallest_value = nil
   smallest_key = nil
-  hash.each do |value, key|
+  hash.each do |key, value|
     if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_key = key
@@ -12,4 +12,4 @@ def key_for_min_value(hash)
   end
   smallest_key
 end
-end
+
